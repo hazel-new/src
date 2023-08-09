92cc3428422f2edc628dda1eb2d48c8663909d35
Hairong Liu <hairong.liu.ext@ericsson.com>
2023-08-09 16:47:55 +0800

    [win]16属性校验

--- 01-base/16-props.js
+++ 01-base/16-props.js
@@ -9,17 +9,17 @@ export default class App extends Component {
       <div>
         <div>
           <h2>首页</h2>
-          <Navbar title="首页" leftshow="false"/>
+          <Navbar title="首页" leftshow={false}/>
         </div>
 
         <div>
           <h2>列表</h2>
-          <Navbar title="列表" leftshow="true"/>
+          <Navbar title="列表" leftshow={true}/>
         </div>
 
         <div>
           <h2>购物车</h2>
-          <Navbar title="购物车" leftshow='false'/>
+          <Navbar title="购物车" leftshow={false}/>
         </div>
       </div>
 
--- 01-base/Navbar/index.js
+++ 01-base/Navbar/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 //命名随意,从React 15.5版本开始，PropTypes由prop-types三方件提供。使用命令npm i -D prop-types安装。React 15.5之前的版本PropTypes由react提供，不需要安装。
-import hazelproptyps from 'prop-types' 
+import PropTypes from 'prop-types' 
 // console.log(hazelproptyps)
 
 export default class Navbar extends Component {
@@ -30,8 +30,8 @@ export default class Navbar extends Component {
 // 类属性，相对的是上面的对象属性（new完才能访问），这个不用new可以直接访问到
 Navbar.prototypes = {
   //title: 验证是不是字符串的方法，
-  title:hazelproptyps.string,
+  title:PropTypes.string,
   // leftshow: 验证是不是bool的方法 
-  leftshow:hazelproptyps.bool
+  leftshow:PropTypes.bool
 
 }
